6f09b990db65484ef0399e5aadd74231d19c963a
Pavel Cistjakov <pavelcistjakov@gmail.com>
2023-01-06 08:42:07 +0000

    Setting up activities and layouts

---
+++ app/src/main/java/android/banananabandit/bestpubs/AddPubActivity.kt
@@ -0,0 +1,11 @@
+package android.banananabandit.bestpubs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_pub)
+    }
+}
