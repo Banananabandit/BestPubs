dd1889f8c0a38c184c6cc9f6183f63095a49451f
Pavel Cistjakov <pavelcistjakov@gmail.com>
2023-01-11 07:26:27 +0000

    Starting work on add activity

--- app/src/main/java/android/banananabandit/bestpubs/AddPubActivity.kt
+++ app/src/main/java/android/banananabandit/bestpubs/AddPubActivity.kt
@@ -1,11 +1,21 @@
 package android.banananabandit.bestpubs
 
+import android.banananabandit.bestpubs.databinding.ActivityAddPubBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class AddPubActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddPubBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_pub)
+        binding = ActivityAddPubBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbarAddPub)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.toolbarAddPub.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
--- app/src/main/java/android/banananabandit/bestpubs/MainActivity.kt
+++ app/src/main/java/android/banananabandit/bestpubs/MainActivity.kt
@@ -1,11 +1,22 @@
 package android.banananabandit.bestpubs
 
+import android.banananabandit.bestpubs.databinding.ActivityMainBinding
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fabAddPub.setOnClickListener {
+            val intent = Intent(this, AddPubActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
